0e3980b31afd51205da45d8df262cabc367cd223
John Yeo <johnyeocx@gmail.com>
2023-11-25 16:26:26 +0000

    fixed run.py to include port

--- assets/fastapi-template/run.py
+++ assets/fastapi-template/run.py
@@ -1,4 +1,9 @@
 import uvicorn
+import sys
 
 if __name__ == "__main__":
-    uvicorn.run("src.main:app", reload=True)
+    uvicorn.run(
+        "src.main:app",
+        reload=True,
+        port=(int(sys.argv[1]) if len(sys.argv) > 1 else 8000),
+    )
